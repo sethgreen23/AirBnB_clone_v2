944c70770ff2b4fe6d2359d0b68e46889470da0a
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-02-21 17:04:40 +0300

    Add solutions for task 9

--- models/place.py
+++ models/place.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.state import State
 from models.city import City
 from models.user import User
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
+
     city_id = Column(String(60), ForeignKey(City.id), nullable=False)
     user_id = Column(String(60), ForeignKey(User.id), nullable=False)
     name = Column(String(128), nullable=False)
@@ -21,3 +22,16 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+    reviews = relationship('Review', cascade='all, delete-orphan',
+                           backref='place')
+    @property
+    def reviews(self, place_id):
+        """getter attribute reviews that returns the list of Review
+        instances with place_id equals to the current Place.id"""
+        from models import storage
+
+        reviews_list = []
+        for place_instance in storage.all():
+            if place_instance.place_id == self.id:
+                reviews_list.append(place_instance)
+        return reviews_list
--- models/user.py
+++ models/user.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
 from models.base_model import BaseModel, Base
-from models.state import State
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
@@ -16,3 +14,5 @@ class User(BaseModel, Base):
     last_name = Column(String(128), nullable=True)
     places = relationship('Place', cascade='all, delete-orphan',
                           backref='user')
+    reviews = relationship('Review', cascade='all, delete-orphan',
+                           backref='user')
