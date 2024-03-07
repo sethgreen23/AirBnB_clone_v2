f0c7af727d86acfa3e646dea562474aced6a78d1
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-03-07 17:57:20 +0300

    Fix pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,15 +7,15 @@ from fabric.api import local
 
 
 def do_pack():
-      """generates a .tgz archive from the contents of the web_static folder"""
-      os.makedirs("versions", exist_ok=True)
-      source_dir = "./web_static"
-      current_time = datetime.datetime.now()
-      timestamp = current_time.strftime("%Y%m%d%H%M%S")
-      output_file = f"web_static_{timestamp}.tgz"
-      if local(f"tar czf {output_file} {source_dir}").failed is True:
-          return None
-      if os.path.exists(output_file):
-            return output_file
-      else:
-         return None
+    """generates a .tgz archive from the contents of the web_static folder"""
+    os.makedirs("versions", exist_ok=True)
+    source_dir = "./web_static"
+    current_time = datetime.datetime.now()
+    timestamp = current_time.strftime("%Y%m%d%H%M%S")
+    output_file = f"web_static_{timestamp}.tgz"
+    if local(f"tar czf {output_file} {source_dir}").failed is True:
+        return None
+    if os.path.exists(output_file):
+        return output_file
+    else:
+        return None
