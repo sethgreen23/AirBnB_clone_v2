1d8938832966c9cd970b76c13f36f77deb785919
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-03-07 16:34:35 -0800

    Add solution for task 5

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,55 +1,54 @@
-# Puppet script that sets up web servers for the deployment of web_static
+# Puppet script to setup web_static delivery for servers
 
-  # Update system
-  exec { 'apt-update':
-    command     => '/usr/bin/apt-get update -y',
-    refreshonly => true,
-  }
-
-  # Install nginx
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  # Create directories
-  file { ['/data/web_static/releases/test', '/data/web_static/shared']:
-    ensure => directory,
-  }
-
-  # Create sample HTML file
-  file { '/data/web_static/releases/test/index.html':
-    content => "<html>\n  <head>\n  </head>\n  <body>\n   ALX Software Engineering\n  </body>\n</html>",
-  }
-
-  # Create symbolic link
-  file { '/data/web_static/current':
-    ensure  => link,
-    target  => '/data/web_static/releases/test',
-    require => File['/data/web_static/releases/test/index.html'],
-  }
-
-  # Give ownership and group of /data to ubuntu
-  file { '/data':
-    ensure  => directory,
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    recurse => true,
-  }
-
-  # Update nginx configuration
-  file { '/etc/nginx/sites-available/default':
-    content => template('web_static_setup/nginx_config.erb'),
-    notify  => Service['nginx'],
-  }
-
-  exec {'update nginx configuration':
-  command => "sed -i '60i\location  /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default",
-  require =>  Service['nginx'],
-  path    => '/bin:/usr/bin',
+# Update system
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update -y',
 }
-  # Restart nginx
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    subscribe => File['/etc/nginx/sites-available/default'],
+
+# Install nginx
+package { 'nginx':
+  ensure => installed,
+}
+
+# Disable firewall
+exec { 'disable-ufw':
+  command => '/usr/sbin/ufw disable',
+}
+
+# Create the directory /data/web_static/releases/test and /data/web_static/shared
+file { '/data/web_static/releases/test':
+  ensure => directory,
+}
+
+file { '/data/web_static/shared':
+  ensure => directory,
+}
+
+# Create a sample html file
+file { '/data/web_static/releases/test/index.html':
+  content => "<html>\n  <head>\n  </head>\n  <body>\n   ALX Software Engineering\n  </body>\n</html>",
+}
+
+# Create symbolic link between /data/web_static/releases/test/ and /data/web_static/current
+file { '/data/web_static/current':
+  ensure => link,
+  target => '/data/web_static/releases/test/',
+  force  => true,
+}
+
+# Give ownership and group of /data to ubuntu
+exec { 'chown-data-directory':
+  command => '/bin/chown -R ubuntu:ubuntu /data/',
+}
+
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+exec { 'update-nginx-config':
+  command => '/bin/bash -c "echo \'location  /hbnb_static { alias /data/web_static/current/;}\' >> /etc/nginx/sites-available/default"',
+  require => File['/etc/nginx/sites-available/default'],
+}
+
+# Restart nginx
+exec { 'restart-nginx':
+  command => '/usr/sbin/service nginx restart',
+  require => Exec['update-nginx-config'],
 }
