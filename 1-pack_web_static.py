c7c7b45077432aa2adb7ee7962fb1c8dedf2f616
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-09 22:37:33 +0100

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,13 +2,14 @@ from fabric.api import local, task
 from datetime import datetime
 import os
 
+
 @task
 def do_pack():
     """ pack the folders """
     d = datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')
     local("mkdir -p versions")
     result = local("tar -cvzf versions/web_static_{}.tgz web_static".format(d),
-          capture=False)
+                   capture=False)
     if not result.failed:
         for _, _, files in os.walk("versions"):
             for file_name in files:
@@ -16,5 +17,3 @@ def do_pack():
                 return rel_file
     else:
         return None
-
-
