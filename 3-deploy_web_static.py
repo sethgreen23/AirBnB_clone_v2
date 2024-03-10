279f36eeaf8aa30e0925c0acf5039d0562c0ae2f
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-10 04:15:15 +0100

    task 3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" Module to pack the files in tar file """
+from fabric.api import local, task, run, put, sudo
+from datetime import datetime
+from fabric.context_managers import env
+import os
+
+
+env.hosts = ['35.175.65.143', '54.146.90.232']
+
+
+@task
+def do_pack():
+    """ pack the folders """
+    d = datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')
+    print("Packing web_static to versions/web_static_{}.tgz".format(d))
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static".format(d),
+                   capture=False)
+    if not result.failed:
+        size = os.path.getsize("versions/web_static_{}.tgz".format(d))
+        form = "web_static packed: versions/web_static_{}.tgz -> {}Bytes"
+        print(form.format(d, size))
+        return "versions/web_static_{}.tgz".format(d)
+    else:
+        return None
+
+
+@task
+def do_deploy(archive_path):
+    """ deploy files """
+    if not os.path.isfile(archive_path):
+        return False
+    f = archive_path.split("/")[1]
+    file_n = f.split(".")[0]
+    dest_path = "/tmp/{}".format(f)
+    put(archive_path, dest_path)
+    run("mkdir -p /data/web_static/releases/{}/".format(file_n))
+    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(f, file_n))
+    run("rm /tmp/{}".format(f))
+    source = "/data/web_static/releases/{}/web_static/*".format(file_n)
+    dest = "/data/web_static/releases/{}/".format(file_n)
+    run("mv {} {}".format(source, dest))
+    run("rm -rf /data/web_static/releases/{}/web_static".format(file_n))
+    run("rm -rf /data/web_static/current")
+    alias = "/data/web_static/current"
+    s_alias = "/data/web_static/releases/{}/".format(file_n)
+    run("ln -s {} {}".format(s_alias, alias))
+    print("New version deployed!")
+    return True
+
+
+@task
+def deploy():
+    """ full deploy """
+    archive_path = do_pack()
+    if not archive_path:
+        return False
+    return do_deploy(archive_path)
