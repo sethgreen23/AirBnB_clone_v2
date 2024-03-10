d09b9f50258d294a705d6246389b24b97b7d91ed
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-10 05:08:17 +0100

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
 """ Module to pack the files in tar file """
-from fabric.api import local, task
+from fabric.api import local, task, run, put, sudo
 from datetime import datetime
+from fabric.context_managers import env
 import os
+
+
+env.hosts = ['35.175.65.143', '54.146.90.232']
 
 
 @task
 def do_pack():
-    """ pack the folders """
-    d = datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')
-    print("Packing web_static to versions/web_static_{}.tgz".format(d))
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
-    result = local("tar -cvzf versions/web_static_{}.tgz web_static".format(d),
-                   capture=False)
-    if not result.failed:
-        size = os.path.getsize("versions/web_static_{}.tgz".format(d))
-        form = "web_static packed: versions/web_static_{}.tgz -> {}Bytes"
-        print(form.format(d, size))
-        return "versions/web_static_{}.tgz".format(d)
-    else:
+    """ pack the folders inside an archive"""
+    try:
+        d = datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')
+        print("Packing web_static to versions/web_static_{}.tgz".format(d))
+        if not os.path.exists("versions"):
+            os.makedirs("versions")
+        com = "tar -cvzf versions/web_static_{}.tgz web_static".format(d)
+        result = local(com, capture=False)
+        if not result.failed:
+            size = os.path.getsize("versions/web_static_{}.tgz".format(d))
+            form = "web_static packed: versions/web_static_{}.tgz -> {}Bytes"
+            print(form.format(d, size))
+            return "versions/web_static_{}.tgz".format(d)
+        else:
+            return None
+    except Exception as e:
         return None
