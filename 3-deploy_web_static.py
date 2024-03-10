33fd5a444a4f52c6394dea9b8830a70cc24e7dfc
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-10 04:59:01 +0100

    task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -11,49 +11,56 @@ env.hosts = ['35.175.65.143', '54.146.90.232']
 
 @task
 def do_pack():
-    """ pack the folders """
-    d = datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')
-    print("Packing web_static to versions/web_static_{}.tgz".format(d))
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
-    result = local("tar -cvzf versions/web_static_{}.tgz web_static".format(d),
-                   capture=False)
-    if not result.failed:
-        size = os.path.getsize("versions/web_static_{}.tgz".format(d))
-        form = "web_static packed: versions/web_static_{}.tgz -> {}Bytes"
-        print(form.format(d, size))
-        return "versions/web_static_{}.tgz".format(d)
-    else:
+    """ pack the folders inside an archive"""
+    try:
+        d = datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')
+        print("Packing web_static to versions/web_static_{}.tgz".format(d))
+        if not os.path.exists("versions"):
+            os.makedirs("versions")
+        com = "tar -cvzf versions/web_static_{}.tgz web_static".format(d)
+        result = local(com, capture=False)
+        if not result.failed:
+            size = os.path.getsize("versions/web_static_{}.tgz".format(d))
+            form = "web_static packed: versions/web_static_{}.tgz -> {}Bytes"
+            print(form.format(d, size))
+            return "versions/web_static_{}.tgz".format(d)
+        else:
+            return None
+    except Exception as e:
         return None
 
 
 @task
 def do_deploy(archive_path):
-    """ deploy files """
+    """ deploy of static files on servers """
     if not os.path.isfile(archive_path):
         return False
-    f = archive_path.split("/")[1]
-    file_n = f.split(".")[0]
-    dest_path = "/tmp/{}".format(f)
-    put(archive_path, dest_path)
-    run("mkdir -p /data/web_static/releases/{}/".format(file_n))
-    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(f, file_n))
-    run("rm /tmp/{}".format(f))
-    source = "/data/web_static/releases/{}/web_static/*".format(file_n)
-    dest = "/data/web_static/releases/{}/".format(file_n)
-    run("mv {} {}".format(source, dest))
-    run("rm -rf /data/web_static/releases/{}/web_static".format(file_n))
-    run("rm -rf /data/web_static/current")
-    alias = "/data/web_static/current"
-    s_alias = "/data/web_static/releases/{}/".format(file_n)
-    run("ln -s {} {}".format(s_alias, alias))
-    print("New version deployed!")
-    return True
+    try:
+        f = archive_path.split("/")[1]
+        file_n = f.split(".")[0]
+        dest_path = "/tmp/{}".format(f)
+        put(archive_path, dest_path)
+        run("mkdir -p /data/web_static/releases/{}/".format(file_n))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(f,
+                                                                       file_n))
+        run("rm /tmp/{}".format(f))
+        source = "/data/web_static/releases/{}/web_static/*".format(file_n)
+        dest = "/data/web_static/releases/{}/".format(file_n)
+        run("mv {} {}".format(source, dest))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(file_n))
+        run("rm -rf /data/web_static/current")
+        alias = "/data/web_static/current"
+        s_alias = "/data/web_static/releases/{}/".format(file_n)
+        run("ln -s {} {}".format(s_alias, alias))
+        print("New version deployed!")
+        return True
+    except Exception as e:
+        return False
 
 
 @task
 def deploy():
-    """ full deploy """
+    """ full deploy of the static files """
     archive_path = do_pack()
     if archive_path is None:
         return False
